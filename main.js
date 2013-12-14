fb928af473af6db6da6313d71705e4b7148d1a2c
Ryosuke Hiramatsu <rhiramat@yahoo-corp.jp>
2013-12-14 23:35:06 +0900

    add iPad DL button

--- main.js
+++ main.js
@@ -4,4 +4,8 @@ $(document).ready( function() {
     $("#dl-btn").click(function(){
         window.location.href = "package/iOS.zip";
     });
+
+    $('#dl-btn-pad').click(function(){
+    	window.location.href ="package/iOS_iPad.zip";
+    })
 });
