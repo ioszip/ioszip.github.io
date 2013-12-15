b480591f4cd822c54c9e703dc821a79449855142
Ryosuke Hiramatsu <rhiramat@yahoo-corp.jp>
2013-12-15 14:37:37 +0900

    integrate dl-btn

--- main.js
+++ main.js
@@ -4,8 +4,4 @@ $(document).ready( function() {
     $("#dl-btn").click(function(){
         window.location.href = "package/iOS.zip";
     });
-
-    $('#dl-btn-pad').click(function(){
-    	window.location.href ="package/iOS_iPad.zip";
-    })
 });
