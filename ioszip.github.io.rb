50486b910f0c36ea43be4c88b2962399a61adf0b
shoma2da <marcos.shoichi.matsuda@gmail.com>
2014-10-11 16:12:28 +0900

    sha1の値を追加

--- ioszip.github.io.rb
+++ ioszip.github.io.rb
@@ -3,7 +3,7 @@ require "formula"
 class IoszipGithubIo < Formula
   homepage "https://github.com/ioszip/ioszip.github.io"
   url "https://github.com/shoma2da/ioszip.github.io/archive/1.0.0.zip"
-  sha1 ""
+  sha1 "114122a1ce3f71dc5cff6d2c1be27a15b40a5da2"
 
   skip_clean 'bin'
 
